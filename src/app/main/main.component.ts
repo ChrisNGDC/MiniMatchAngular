2b4361e5db0416fae08b9398ed6cd4aa90270d20
Christian Gabor <chrisgabor95@gmail.com>
2024-11-20 20:51:43 -0300

    arreglo de conflictos

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  RouterLink,
-  RouterLinkActive,
-  ActivatedRoute,
-  Router,
-} from '@angular/router';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { PuntuacionesService } from '../services/puntuaciones.service';
 import { AuthService } from '../services/AuthService';
 
@@ -26,11 +21,9 @@ export class MainComponent implements OnInit {
   puestos: string[] = ['🥇', '🥈', '🥉', '4', '5', '6', '7', '8', '9', '10'];
   juegos: string[] = ['tateti', 'ppt', 'memoria', 'snake', 'tetris'];
   logueado: string;
-  private sub: any;
-  public isLoggedIn: boolean = false;  
+  public isLoggedIn: boolean = false;
 
   constructor(
-    private route: ActivatedRoute,
     private router: Router,
     private puntuacionesService: PuntuacionesService,
     private authService: AuthService
@@ -39,19 +32,22 @@ export class MainComponent implements OnInit {
   /*ngOnDestroy() {
     this.sub.unsubscribe();
   }*/
-  ngOnInit() {       
-    this.cargarRankings();     
+  ngOnInit() {
+    this.cargarRankings();
     this.authService.isLoggedIn$.subscribe((state) => {
       this.isLoggedIn = state;
     });
+  }
+
   organizarRankings() {
     this.rankings.forEach((juego) => {
       juego.puntuaciones = juego.puntuaciones
         .sort((a: any, b: any) => b.score - a.score)
         .splice(0, 10);
     });
-    console.log(this.rankings)
+    console.log(this.rankings);
   }
+
   async cargarRankings() {
     this.rankings = [];
     let response: any;
@@ -68,18 +64,18 @@ export class MainComponent implements OnInit {
     });
   }
 
-  ranking_actual = 0;  
+  ranking_actual = 0;
 
   toggleLogin() {
     if (this.isLoggedIn) {
       this.authService.signOut();
-      this.authService.logout();       
+      this.authService.logout();
     } else {
       this.authService.login();
-      this.router.navigate(['/login']);      
+      this.router.navigate(['/login']);
     }
-  } 
-  
+  }
+
   cambiarRanking(cantidad: number) {
     if (this.ranking_actual + cantidad > this.rankings.length - 1) {
       this.ranking_actual = 0;
@@ -89,8 +85,4 @@ export class MainComponent implements OnInit {
       this.ranking_actual += cantidad;
     }
   }
-
-
-
-  
 }
