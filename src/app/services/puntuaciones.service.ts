6ff6ff15a892db8fbb30ad72c9f7329f01524bdd
Christian Gabor <chrisgabor95@gmail.com>
2024-11-21 11:55:02 -0300

    .

--- src/app/services/puntuaciones.service.ts
+++ src/app/services/puntuaciones.service.ts
@@ -20,15 +20,12 @@ export class PuntuacionesService {
       this.auth.getCurrentUserFullName().then((username) => {
         console.log(user);
         if (user) {
-          const options = {
-            headers: new HttpHeaders().set('Access-Control-Allow-Origin:', '*')
-          };
           const body = {
             username: username,
             score: score,
             id: user.userId,
           };
-          this.http.post('https://j3o3czta78.execute-api.us-east-1.amazonaws.com/scores/' + game, body, options)
+          this.http.post('https://j3o3czta78.execute-api.us-east-1.amazonaws.com/scores/' + game, body)
             .subscribe({
               next: (response) => {
                 console.log(response);
